e6c02a247011daff516539f204cda78a952d8b6d
Tao Huang <ht.anglenx@gmail.com>
2014-10-17 17:08:52 +0800

    intersect

--- splitpython.py
+++ splitpython.py
@@ -7,16 +7,67 @@ shapefieldname = desc.ShapeFieldName
 
 rows = arcpy.SearchCursor(infc)
 
-def generate_vector(ul,ur,ll,lr,direction):
-    pass
+def symbol(num):
+    if num >= 0:
+        return 1
+    else:
+        return -1
+# y = ax + b
+def get_b(line):
+    start = line[0]
+    slope = slope(line)
+    b = slope * start.X - start.Y
+
+def slope(line):
+    start = line[0]
+    end = line[1]
+    return (end.Y - start.Y) / (end.X - start.X)
+
+def touch(line1,line2):
+    start1 = line1[0]
+    end1   = line1[1]
+
+    start2 = line2[0]
+    slope2 = slope(line2)
+    b2 = slope2 * start2.X - start2.Y
+
+    func  = lambda x,y : slope2 * x + b2 -y
+
+    return symbol(func(start1.X,start1.Y)) == symbol(func(end1.X,end1.Y)))
+
+
+def intersect(line1,line2):
+    if touch(line1,line2): return None
+    a,b = line1[0],line1[1]
+    c,d = line2[0],line2[1]
+    denominator = (b.Y - a.Y)*(d.X - c.X) - (a.X - b.X)*(c.Y - d.Y)
+    x = ( (b.X - a.X) * (d.X - c.X) * (c.Y - a.Y)
+                + (b.Y - a.Y) * (d.X - c.X) * a.X
+                - (d.Y - c.Y) * (b.X - a.X) * c.X ) / denominator
+    y = -( (b.Y - a.Y) * (d.Y - c.Y) * (c.X - a.X)
+                + (b.X - a.X) * (d.Y - c.Y) * a.Y
+                - (d.X - c.X) * (b.Y - a.Y) * c.Y ) / denominator
+    return arcpy.Point(x,y)
+
+def vector_cross(vector1,vector2):
+    return (vector1.Y * vector2.Z - vector1.Z * vector2.Y,
+            vector1.Z * vector2.X - vector1.X * vector2.Z,
+            vector1.X * vector2.Y - vector1.Y * vector2.X)
+
+def vector_dot(vector1,vector2):
+    return vector1.X * vector2.X + vector1.Y + vector2.Y
+
+def start_end(extent,direction):
+    ul = extent.upperLeft
+    ur = extent.upperRight
+    lr = extent.lowerRight
+    ll = extent.lowerLeft
+    start_pnts = [ul,ur,lr,ll]
+    end_pnts  = [ur,lr,ll,ul]
+    lines = zip(start,end)
 
 def generate(extent,direction):
-    ur = extent.upperRight
-    ul = extent.upperLeft
-    ll = extent.lowerLeft
-    lr = extent.lowerRight
-
-    start,end = generate_vector(ul,ur,ll,lr,direction)
+    start,end = start_end(extent,direction)
     array = arcpy.Array()
     array.add(start)
     array.add(end)
